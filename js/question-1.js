97eb9847fde2caf3bf0b2576d80d58e98a71b945
Eirik Berget Haukeland <eirik@haukeland.name>
2022-11-06 12:48:09 +0100

    added example of multi line arrow funtion

--- js/question-1.js
+++ js/question-1.js
@@ -2,6 +2,16 @@
 //   return a % b;
 // }
 
+// for multi line functions
+
+// eslint-disable-next-line arrow-body-style
+const doModulus = (a, b) => {
+  return a % b;
+};
+
+// for single line fiunctions
+
 const getRemainder = (a, b) => a % b;
 
 getRemainder(99, 30);
+doModulus(50, 40);
