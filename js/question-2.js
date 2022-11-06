0982f27b92f3e6d00d5699befaebf4ab402bf85a
Eirik Berget Haukeland <eirik@haukeland.name>
2022-11-06 12:50:09 +0100

    ajusted to html change and added try, catch block

--- js/question-2.js
+++ js/question-2.js
@@ -1,13 +1,10 @@
 /* eslint-disable no-undef */
 /* eslint-disable quotes */
 
-const body = document.querySelector("body");
+const apiResault = document.querySelector("#api-resault");
 
-const callRawgApi = async (key) => {
-  const request = await fetch(`https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating&key=${key}`);
-  const response = await request.json();
-
-  for (let i = 0; i < response.results.length; i += 1) {
+const renderGames = (games) => {
+  for (let i = 0; i < games.length; i += 1) {
     if (i === 8) {
       break;
     }
@@ -16,27 +13,45 @@ const callRawgApi = async (key) => {
     game.classList += "game-card";
 
     const gameName = document.createElement("h2");
-    gameName.innerText = response.results[i].name;
+    gameName.innerText = games[i].name;
     game.appendChild(gameName);
 
     const gameRating = document.createElement("span");
-    if (!response.results[i].rating) {
+    if (!games[i].rating) {
       gameRating.innerText = `rating are not awailable for this game`;
     } else {
-      gameRating.innerText = `the rating is ${response.results[i].rating}`;
+      gameRating.innerText = `the rating is ${games[i].rating}`;
     }
     game.appendChild(gameRating);
 
     const gameTags = document.createElement("span");
-    if (!response.results[i].tags) {
+    if (!games[i].tags) {
       gameTags.innerText = `tags are not awailable for this game`;
     } else {
-      gameTags.innerText = `this game has ${response.results[i].tags.length} tags`;
+      gameTags.innerText = `this game has ${games[i].tags.length} tags`;
     }
     game.appendChild(gameTags);
 
-    body.appendChild(game);
+    apiResault.appendChild(game);
   }
 };
 
+const addError = (message) => {
+  const displayError = document.createElement("span");
+  displayError.classList.add("error");
+  displayError.innerText = message;
+  apiResault.appendChild(displayError);
+};
+
+const callRawgApi = async (key) => {
+  try {
+    const request = await fetch(`https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating&key=${key}g`);
+    const data = await request.json();
+    renderGames(data.results);
+  } catch (e) {
+    addError(e);
+  }
+};
+
+// to protect the secret this fetch shuld be in a backend if it is to be protected
 callRawgApi("c8168de48ef145e6a2ba1edd05455dd6");
